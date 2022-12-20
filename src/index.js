39d6217d62345f5b2318c3f4dc076b3df760ba6e
Vinicius1 <vmsilva1227@gmail.com>
2022-12-20 19:00:39 -0300

    Introducing the deposit function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -8,6 +9,23 @@ app.use(express.json());
 const customers = [];
 
 //Middleware
+function verifyIfCostumersAlreadyExists(req, res, next) {
+  const { cpf, name } = req.body;
+
+  const costumersAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (costumersAlreadyExists) {
+    return res.status(400).json({ error: "Costumer already exists!" });
+  }
+
+  req.cpf = cpf;
+  req.name = name;
+
+  return next();
+}
+
 function verifyIfExistsAccountCPF(req, res, next) {
   const { cpf } = req.headers;
 
@@ -22,16 +40,8 @@ function verifyIfExistsAccountCPF(req, res, next) {
   return next();
 }
 
-app.post("/account", (req, res) => {
-  const { cpf, name } = req.body;
-
-  const costumersAlreadyExists = customers.some(
-    (customer) => customer.cpf === cpf
-  );
-
-  if (costumersAlreadyExists) {
-    return res.status(400).json({ error: "Costumer already exists!" });
-  }
+app.post("/account", verifyIfCostumersAlreadyExists, (req, res) => {
+  const { cpf, name } = req;
 
   customers.push({
     cpf,
@@ -49,6 +59,23 @@ app.get("/statement", verifyIfExistsAccountCPF, (req, res) => {
   const { customer } = req;
 
   return res.send(customer.statement);
+});
+
+app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
+  const { description, amount } = req.body;
+
+  const { customer } = req;
+
+  const statementOperation = {
+    description,
+    amount,
+    created_at: new Date(),
+    type: "credit",
+  };
+
+  customer.statement.push(statementOperation);
+
+  return res.status(201).send();
 });
 
 app.listen(3333);
