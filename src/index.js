671805719790649cb95131336aee9e5c5c3382d8
Vinicius1 <vmsilva1227@gmail.com>
2022-12-20 19:36:52 -0300

    Introducing the date and withdraw functions

--- src/index.js
+++ src/index.js
@@ -40,6 +40,18 @@ function verifyIfExistsAccountCPF(req, res, next) {
   return next();
 }
 
+function getBalance(statement) {
+  const balance = statement.reduce((acc, operation) => {
+    if (operation.type === "credit") {
+      return acc + operation.amount;
+    } else {
+      return acc - operation.amount;
+    }
+  }, 0);
+
+  return balance;
+}
+
 app.post("/account", verifyIfCostumersAlreadyExists, (req, res) => {
   const { cpf, name } = req;
 
@@ -76,6 +88,61 @@ app.post("/deposit", verifyIfExistsAccountCPF, (req, res) => {
   customer.statement.push(statementOperation);
 
   return res.status(201).send();
+});
+
+app.post("/withdraw", verifyIfExistsAccountCPF, (req, res) => {
+  const { amount } = req.body;
+  const { customer } = req;
+
+  const balance = getBalance(customer.statement);
+
+  if (balance < amount) {
+    res.status(400).json({ error: "Insufficient funds!" });
+  }
+
+  const statementOperation = {
+    amount,
+    created_at: new Date(),
+    type: "debit",
+  };
+
+  customer.statement.push(statementOperation);
+  return res.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+  const { date } = req.query;
+
+  const dateFormat = new Date(date + " 00:00");
+
+  const statement = customer.statement.filter(
+    (statement) =>
+      statement.created_at.toDateString() ===
+      new Date(dateFormat).toDateString()
+  );
+
+  return res.json(statement);
+});
+
+app.put(
+  "/account",
+  verifyIfCostumersAlreadyExists,
+  verifyIfExistsAccountCPF,
+  (req, res) => {
+    const { name } = req;
+    const { customer } = req;
+
+    customer.name = name;
+
+    return res.status(201).send();
+  }
+);
+
+app.get("/account", verifyIfExistsAccountCPF, (req, res) => {
+  const { customer } = req;
+
+  return res.json(customer);
 });
 
 app.listen(3333);
